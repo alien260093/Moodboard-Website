06f8c66ad74fe94c7d7af4d96bb0cb87e9450d36
lil alien <liutianna310@gmail.com>
2025-09-02 22:14:05 +1000

    yes

--- main.py
+++ main.py
@@ -8,13 +8,8 @@ app = Flask(__name__)
 @app.route('/index.html', methods=['GET'])
 @app.route('/', methods=['POST', 'GET'])
 def index():
-  return render_template('index.html')
+    data = dbHandler.listExtension()
+    return render_template('index.html', content=data)
 
 if __name__ == '__main__':
-  app.run(debug=True, host='0.0.0.0', port=5000)
-
-
-def index():
-   data = dbHandler.listExtension()
-   return render_template('/index.html', content=data)
-   
+    app.run(debug=True, host='0.0.0.0', port=5000)
