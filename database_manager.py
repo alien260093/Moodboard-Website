8f89a0e2751573c0cd6e1d5956b99cd9169bdcf2
lil alien <liutianna310@gmail.com>
2025-09-02 22:19:19 +1000

    yes

--- database_manager.py
+++ database_manager.py
@@ -1,8 +1,8 @@
 import sqlite3 as sql
 
 def listExtension():
-  con = sql.connect("database/data_source.db")
+  con = sql.connect("Database/data_source.db")
   cur = con.cursor()
-  data = cur.execute('SELECT * FROM extension').fetchall()
+  data = cur.execute('SELECT * FROM Users').fetchall()
   con.close()
   return data
