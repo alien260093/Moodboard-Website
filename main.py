c2d2f8526180c8830843acc36e8b83bca1bcd9e3
lil alien <liutianna310@gmail.com>
2025-09-02 21:49:03 +1000

    fixed webpage appearance issues

--- main.py
+++ main.py
@@ -12,3 +12,9 @@ def index():
 
 if __name__ == '__main__':
   app.run(debug=True, host='0.0.0.0', port=5000)
+
+
+def index():
+   data = dbHandler.listExtension()
+   return render_template('/index.html', content=data)
+   
